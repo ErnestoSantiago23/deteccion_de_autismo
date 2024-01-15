68fad73b3bddc2b530dcbaf070d50307ce8b26fd
ernesto santiago <santiagoernesto352@gmail.com>
2024-01-15 12:23:42 -0300

    Mejora 3 Frontend

--- pages/spectrumInsight.py
+++ pages/spectrumInsight.py
@@ -32,20 +32,21 @@ preguntas = ["Question 1: Choose the child's age range in months",
              "Question 14: Does your child stare at nothing with no apparent purpose?"]
 
 
-answer_5_and_11 = ["Always", "Usually", "Sometimes", "Rarely", "Never"]
-answer_7_questions = ["Many times a day", "A few times a day", "A few times a week", "Less than once a week", "Never"]
-answer_6 = ["Very easy", "Quite easy", "Quite difficult", "Very difficult", "Impossible"]
-answer_12 = ["Very typical", "Quite typical", "Slightly unusual", "Very unusual", "My child doesn't speak"]
-answer_1 = ["12-18", "18-24", "24-30", "30-36"]
-answer_2 = ["Male", "Female"]
-answer_3 = ['Middle eastern', 'White european', 'Hispanic', 'Black', 'Asian', 'South asian', 'Native indian', 'Pacifica', 'Latino', 'Mixed', 'Others']
-answer_4 = ["Yes", "No"]
+answer_5_and_11 = ["--","Always", "Usually", "Sometimes", "Rarely", "Never"]
+answer_7_questions = ["--","Many times a day", "A few times a day", "A few times a week", "Less than once a week", "Never"]
+answer_6 = ["--","Very easy", "Quite easy", "Quite difficult", "Very difficult", "Impossible"]
+answer_12 = ["--","Very typical", "Quite typical", "Slightly unusual", "Very unusual", "My child doesn't speak"]
+answer_1 = ["--","12-18", "18-24", "24-30", "30 or more"]
+answer_2 = ["--","Male", "Female"]
+answer_3 = ["--",'Middle eastern', 'White european', 'Hispanic', 'Black', 'Asian', 'South asian', 'Native indian', 'Pacifica', 'Latino', 'Mixed', 'Others']
+answer_4 = ["--","Yes", "No"]
+
 
 mapeo_respuestas = {"Always": 0, "Sometimes": 1, "Never": 1, "Rarely": 1, "Usually": 0,
                     "Many times a day": 0, "A few times a day": 0, "A few times a week": 1, "Less than once a week": 1,
                     "Very easy": 0, "Quite easy": 0, "Quite difficult": 1, "Very difficult": 1, "Impossible": 1,
                     "Very typical": 0, "Quite typical": 0, "Slightly unusual": 1, "Very unusual": 1,
-                    "My child doesn't speak": 0, "12-18": 0, "18-24": 1, "24-30": 2, "30-36": 3,
+                    "My child doesn't speak": 0, "12-18": 0, "18-24": 1, "24-30": 2, "30 or more": 3,
                     "Male": 1, "Female": 0, "Yes": 1, "No": 0, "Hispanic": 0,
                     "White european": 5, "Middle eastern": 8, "Black": 7, "Asian": 6, "South asian": 10,
                     "Native indian": 2, "Mixed": 9, "Latino": 1, "Pacifica": 4, "Others": 3}
@@ -112,24 +113,37 @@ for i, pregunta in enumerate(preguntas):
 
 # Botón de predicción
 if st.button("Predict"):
-    # Realizar la predicción usando tu modelo
-    prediction =predict(model, np.array(respuestas_usuario).reshape(1,-1))
-    proba = model.predict_proba(np.array(respuestas_usuario).reshape(1,-1))
-
-    # Mostrar el resultado
-    if prediction == 1:
-        st.write("There is a {:.2%} chance that the child has autism".format(proba[0, 1]))
+    # Verificar si todas las preguntas fueron respondidas
+    if "--" in respuestas_usuario:
+        # Mostrar mensaje de error si alguna pregunta no fue respondida
+        st.error("Please answer all the questions before proceeding.")
     else:
-        st.write("There is a {:.2%} chance that the child does not have autism".format(proba[0, 0]))
+        # Realizar la predicción usando tu modelo
+        prediction =predict(model, np.array(respuestas_usuario).reshape(1,-1))
+        proba = model.predict_proba(np.array(respuestas_usuario).reshape(1,-1))
 
-    st.write("**Please do not take these results as infallible. Go to the doctor, if you have not already done so, and verify these results with a professional. In the graph below you can see the possibilities for each case.**")
+        # Mostrar el resultado
+        if prediction == 1:
+            st.write("There is a {:.2%} chance that the child has autism".format(proba[0, 1]))
+        else:
+            st.write("There is a {:.2%} chance that the child does not have autism".format(proba[0, 0]))
 
-    classes = model.classes_
-    fig, ax = plt.subplots()
-    ax.bar([0, 1], proba.flatten()*100, tick_label=['No', 'Yes'], color=['lightcoral', 'lightgreen'], edgecolor='black')
-    ax.set_xlabel('Classes')
-    ax.set_ylabel('Probabilities')
-    ax.set_title('Probabilities for Each Class')
-    ax.set_facecolor('#04B2D9')
-    fig.set_facecolor('#04B2D9')
-    st.pyplot(fig)
+        st.write("**Please do not take these results as infallible. Go to the doctor, if you have not already done so, and verify these results with a professional. In the graph below you can see the possibilities for each case.**")
+
+        proba_flattened = proba.flatten() * 100
+
+        classes = model.classes_
+        fig, ax = plt.subplots()
+        bars = ax.bar([0, 1], proba_flattened, tick_label=['No', 'Yes'], color=['lightcoral', 'lightgreen'], edgecolor='black')
+        ax.set_xlabel('Classes')
+        ax.set_ylabel('Probabilities')
+        ax.set_title('Probabilities for Each Class')
+        ax.set_facecolor('#04B2D9')
+        fig.set_facecolor('#04B2D9')
+
+        # Añadir etiquetas de porcentaje en las barras
+        for bar in bars:
+            yval = bar.get_height()
+            ax.text(bar.get_x() + bar.get_width() / 2.0, yval + 1, f"{yval:.2f}%", ha='center', va='bottom')
+
+        st.pyplot(fig)
